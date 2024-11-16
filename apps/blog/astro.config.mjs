85ec4010f0d52196cd98b39819d40a59fbe21c1b
Narration SD <narration-sd@users.noreply.github.com>
2024-11-16 13:59:18 -0800

    try Maaz's alias to lodash-es

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -37,6 +37,7 @@ export default defineConfig({
     }),
     react(),
   ],
+  vite: { resolve: { alias: { lodash : 'lodash-es' } } },
   output: "server",
   adapter: vercel({
     edgeMiddleware: true,
