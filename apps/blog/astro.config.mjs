f4f7db0b8c6e9ea1846846873abd27509c6d43d8
Narration SD <narration-sd@users.noreply.github.com>
2025-02-20 13:22:24 -0800

    Initial Cloudflare version, builds, operates through wrangler dev via pnpm run preview. Note the workaround in vite config

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -1,14 +1,14 @@
 import sanityIntegration from "@sanity/astro";
 import { defineConfig } from "astro/config";
-import node from '@astrojs/node';
 import react from "@astrojs/react";
-import { loadEnv } from 'vite'
+import { loadEnv } from 'vite';
+import cloudflare from "@astrojs/cloudflare";
 
 const env = {
   ...process.env,
   ...loadEnv(process.env.NODE_ENV, process.cwd(), [
     'PUBLIC_SANITY_', // can add others
-  ])}
+])}
 
 // 'sanity' check...
 if (!(env.PUBLIC_SANITY_PROJECT_ID && env.PUBLIC_SANITY_DATASET
@@ -22,26 +22,32 @@ if (!(env.PUBLIC_SANITY_PROJECT_ID && env.PUBLIC_SANITY_DATASET
 export default defineConfig({
   integrations: [
     sanityIntegration({
-      projectId: env.PUBLIC_SANITY_PROJECT_ID,
-      dataset: env.PUBLIC_SANITY_DATASET,
-      apiVersion: env.PUBLIC_SANITY_API_VERSION,
-      useCdn: false,
-      perspective: 'drafts',
-      token: env.PUBLIC_SANITY_VIEWER_TOKEN,
-      stega: {
-        enabled:true,
+    projectId: env.PUBLIC_SANITY_PROJECT_ID,
+    dataset: env.PUBLIC_SANITY_DATASET,
+    apiVersion: env.PUBLIC_SANITY_API_VERSION,
+    useCdn: false,
+    perspective: 'drafts',
+    token: env.PUBLIC_SANITY_VIEWER_TOKEN,
+    stega: {
+      enabled: true,
         studioUrl: env.PUBLIC_SANITY_STUDIO_PREVIEW_URL
             + env.PUBLIC_SANITY_STUDIO_BASE_PATH,
-      },
+    },
       studioBasePath: /*env.PUBLIC_SANITY_STUDIO_PREVIEW_URL + */ env.PUBLIC_SANITY_STUDIO_BASE_PATH,
     }),
     react(),
   ],
-  vite: { resolve: { alias: { lodash : 'lodash-es' } } },
+  vite: {
+    resolve: {
+      alias: {
+        lodash : 'lodash-es',
+        // this next required workaround for Astro bug still present
+        "react-dom/server": "react-dom/server.edge",
+      }
+    }
+  },
   output: "server",
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: cloudflare()
 });
 
 // n.b. pnpm run build normally, then run the result,
