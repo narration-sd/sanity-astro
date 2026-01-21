4431f3d755f0260e5b879b9ab6686a2b3f52e332
Narration SD <narration-sd@users.noreply.github.com>
2026-01-20 22:42:00 -0800

    We are on Vercel successfully now, but Sanity now needs this deployment config added for the studio to operate in the dashboard.

--- apps/blog/sanity.cli.ts
+++ apps/blog/sanity.cli.ts
@@ -1,9 +1,13 @@
 // sanity.cli.js
-import { defineCliConfig } from "sanity/cli";
+import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: process.env.PUBLIC_SANITY_PROJECT_ID,
     dataset: process.env.PUBLIC_SANITY_DATASET,
+  },
+  deployment: {
+    appId: 'ivirq7br6j1p6ytxbj65o1ep',
+    autoUpdates: true,
   }
-});
+})
